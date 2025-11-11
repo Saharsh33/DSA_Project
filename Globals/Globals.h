1396b1714ffda33a8fccae0746ee37b55f00c45c
pal-vaghasiya <b24cs1078@iitj.ac.in>
2025-11-12 01:20:00 +0530

    global variable added

--- Globals/Globals.h
+++ Globals/Globals.h
@@ -13,5 +13,18 @@ struct Song//song structure
     long long play_count;
     double trending_score;
 };
+//global variables
+extern vector<Song> songs;
+extern string songsFile;
+extern string playlistFile;
+extern string graphCacheFile;
+extern unordered_map<string, vector<int>> playlists;
+
+extern unordered_map<string, int> genreIndex;
+extern unordered_map<string, int> artistIndex;
+extern int GENRE_DIM;
+extern int ARTIST_DIM;
+
+extern unordered_map<int, vector<pair<int, double>>> graph;
 
 #endif
